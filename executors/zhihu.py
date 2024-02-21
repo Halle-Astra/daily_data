53ac701ec54c9ac756f6a5ad020c0e28031a19b0
halle-astra <12535232+halle-astra@user.noreply.gitee.com>
2024-02-21 15:08:53 +0800

    get the question info successfully

--- executors/zhihu.py
+++ executors/zhihu.py
@@ -3,6 +3,7 @@
 from clients.requests_client import Client as RqClient
 from loguru import logger
 from .core import BaseExecutor
+from lxml import etree
 
 class  Executor_v1(BaseExecutor):
     def __init__(self, **kwargs):
@@ -15,7 +16,19 @@ class  Executor_v1(BaseExecutor):
 
     #def get_home(self):
     def __call__(self):
-        super().run_primary()
+        answer_urls = super().run_primary()
+        for answer_url in answer_urls:
+            answer_url = answer_url[2:]
+            question_url = 'https://' + '/'.join(answer_url.split('/')[:3])
+            question_init_web = self.rq_client.get(question_url)
+            if question_init_web.status_code == 200:
+                question_init_web.encoding = question_init_web.apparent_encoding
+                q_init_html = etree.HTML(question_init_web.text)
+                q_info = q_init_html.xpath('//script[@id="js-initialData"]/text()') # 会得到一大堆东西
+                logger.info(q_info )
+
+
+
 
     def start(self):
         self.__call__()
